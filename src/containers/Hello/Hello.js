37fd4122e423dd5a18c2c6e550ed37b839de6658
Pascal Scheidegger <paschcua@hispeed.ch>
2016-07-27 21:05:09 +0200

    postgre test

--- src/containers/Hello/Hello.js
+++ src/containers/Hello/Hello.js
@@ -3,6 +3,26 @@ import Helmet from 'react-helmet';
 
 export default class Hello extends Component {
 
+  componentDidMount() {
+      setTimeout(() => {
+
+        var pg = require('pg');
+
+        pg.defaults.ssl = true;
+        pg.connect(process.env.DATABASE_URL, function(err, conn) {
+          if (err) throw err;
+          console.log('Connected to postgres! Getting schemas...');
+
+          conn
+            .query('SELECT table_schema,table_name FROM information_schema.tables;')
+            .on('row', function(row) {
+              console.log(JSON.stringify(row));
+            });
+        });
+
+      }, 100);
+  }
+
   render() {
     return (
       <div className="container">
