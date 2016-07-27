b4127900a5753cb38fb6a662d98d51051caf1f81
Pascal Scheidegger <paschcua@hispeed.ch>
2016-07-27 22:51:03 +0200

    postgre remove

--- src/containers/Hello/Hello.js
+++ src/containers/Hello/Hello.js
@@ -1,28 +1,7 @@
 import React, {Component} from 'react';
 import Helmet from 'react-helmet';
-import fs from 'fs';
 
 export default class Hello extends Component {
-
-  componentDidMount() {
-      setTimeout(() => {
-
-        var pg = require('pg');
-
-        pg.defaults.ssl = true;
-        pg.connect(process.env.DATABASE_URL, function(err, conn) {
-          if (err) throw err;
-          console.log('Connected to postgres! Getting schemas...');
-
-          conn
-            .query('SELECT table_schema,table_name FROM information_schema.tables;')
-            .on('row', function(row) {
-              console.log(JSON.stringify(row));
-            });
-        });
-
-      }, 100);
-  }
 
   render() {
     return (
