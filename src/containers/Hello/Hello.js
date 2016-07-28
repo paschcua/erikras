b1e8ae23679db07e02f3185182bc6b3192839f9b
sdp <Cobra1985>
2016-07-28 18:41:48 +0200

    mongodb test2

--- src/containers/Hello/Hello.js
+++ src/containers/Hello/Hello.js
@@ -3,64 +3,66 @@ import Helmet from 'react-helmet';
 
 var mongoose = require('mongoose');
 
-var db = mongoose.connection;
-
-db.on('error', console.error);
-db.once('open', function() {
-
-  var movieSchema = new mongoose.Schema({
-    title: { type: String }
-  , rating: String
-  , releaseYear: Number
-  , hasCreditCookie: Boolean
-  });
-
-  // Compile a 'Movie' model using the movieSchema as the structure.
-  // Mongoose also creates a MongoDB collection called 'Movies' for these documents.
-  var Movie = mongoose.model('Movie', movieSchema);
-
-  var thor = new Movie({
-    title: 'Thor'
-  , rating: 'PG-13'
-  , releaseYear: '2011'  // Notice the use of a String rather than a Number - Mongoose will automatically convert this for us.
-  , hasCreditCookie: true
-  });
-
-  thor.save(function(err, thor) {
-    if (err) return console.error(err);
-    console.dir(thor);
-  });
-
-
-
-
-
-
-  // Find a single movie by name.
-  Movie.findOne({ title: 'Thor' }, function(err, thor) {
-    if (err) return console.error(err);
-    console.dir(thor);
-  });
-
-  // Find all movies.
-  Movie.find(function(err, movies) {
-    if (err) return console.error(err);
-    console.dir(movies);
-  });
-
-  // Find all movies that have a credit cookie.
-  Movie.find({ hasCreditCookie: true }, function(err, movies) {
-    if (err) return console.error(err);
-    console.dir(movies);
-  });
-
-});
-
-mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
-
-
-
 export default class Hello extends Component {
+
+
+componentDidMount() {
+  var db = mongoose.connection;
+
+  db.on('error', console.error);
+  db.once('open', function() {
+
+    var movieSchema = new mongoose.Schema({
+      title: { type: String }
+    , rating: String
+    , releaseYear: Number
+    , hasCreditCookie: Boolean
+    });
+
+    // Compile a 'Movie' model using the movieSchema as the structure.
+    // Mongoose also creates a MongoDB collection called 'Movies' for these documents.
+    var Movie = mongoose.model('Movie', movieSchema);
+
+    var thor = new Movie({
+      title: 'Thor'
+    , rating: 'PG-13'
+    , releaseYear: '2011'  // Notice the use of a String rather than a Number - Mongoose will automatically convert this for us.
+    , hasCreditCookie: true
+    });
+
+    thor.save(function(err, thor) {
+      if (err) return console.error(err);
+      console.dir(thor);
+    });
+
+
+
+
+
+
+    // Find a single movie by name.
+    Movie.findOne({ title: 'Thor' }, function(err, thor) {
+      if (err) return console.error(err);
+      console.dir(thor);
+    });
+
+    // Find all movies.
+    Movie.find(function(err, movies) {
+      if (err) return console.error(err);
+      console.dir(movies);
+    });
+
+    // Find all movies that have a credit cookie.
+    Movie.find({ hasCreditCookie: true }, function(err, movies) {
+      if (err) return console.error(err);
+      console.dir(movies);
+    });
+
+  });
+
+  mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
+}
+
 
   render() {
     return (
