7eac2df9eee67326859eb7d30a4971743adb5f1a
Pascal Scheidegger <paschcua@hispeed.ch>
2016-08-04 23:20:49 +0200

    mongoose server.js working static

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -1,6 +1,5 @@
 import React, {Component, PropTypes} from 'react';
 import Helmet from 'react-helmet';
-import mongoose from 'mongoose';
 
 export default class Register extends Component {
 
@@ -10,33 +9,6 @@ export default class Register extends Component {
     logout: PropTypes.func
   }
 
-  mongoInsert() {
-    var mongoose = require('mongoose');
-    var db = mongoose.connection;
-
-    db.on('error', console.error);
-    db.once('open', function() {
-    });
-    mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
-
-    var userSchema = new mongoose.Schema({
-      userid: Number,
-      username: String,
-      password: String
-    });
-
-    var UserModel = mongoose.model('User', userSchema);
-    var UserData = new UserModel({
-      userid: 2,
-      username: "inputUsername455",
-      password: "inputPassword455"
-    });
-
-    UserData.save(function (err) {
-      if (err) return console.log(err);
-    });
-  }
-
   handleSubmit = (event) => {
     event.preventDefault();
     /* {
@@ -44,7 +16,6 @@ export default class Register extends Component {
     const inputUsername = this.refs.username.value;
     const inputPassword = this.refs.password.value;
 } */
-    this.mongoInsert();
   }
 
   render() {
