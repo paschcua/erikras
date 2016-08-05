1fe6d4ae793356f059797e751a5de42b23a39ecb
sdp <Cobra1985>
2016-08-05 09:41:45 +0200

    final test mongoose WOKRING DEF

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -1,6 +1,5 @@
 import React, {Component, PropTypes} from 'react';
 import Helmet from 'react-helmet';
-import mongoose from '../../models/MongoCall/MongoCall';
 
 export default class Register extends Component {
 
@@ -10,13 +9,42 @@ export default class Register extends Component {
     logout: PropTypes.func
   }
 
+  mongoInitial() {
+    var mongoose = require('mongoose');
+    var db = mongoose.connection;
+
+    db.on('error', console.error);
+    db.once('open', function() {
+    });
+    mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
+
+    var userSchema = new mongoose.Schema({
+      userid: Number,
+      username: String,
+      password: String
+    });
+
+    var UserModel = mongoose.model('User', userSchema);
+  }
+
+  mongoSave(inputUsername, inputPassword) {
+  var UserData = new UserModel({
+    userid: 2,
+    username: inputUsername,
+    password: inputPassword
+  });
+
+  UserData.save(function (err) {
+    if (err) return console.log(err);
+  });
+  }
+
   handleSubmit = (event) => {
     event.preventDefault();
-    /* {
     this.mongoInitial();
     const inputUsername = this.refs.username.value;
     const inputPassword = this.refs.password.value;
-} */
+    this.mongoSave(inputUsername, inputPassword);
   }
 
   render() {
