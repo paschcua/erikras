a50fc9e5feedc8ab7685a3fb7727e886497b06d0
sdp <Cobra1985>
2016-08-05 16:22:36 +0200

    final MONGOOSE459saa

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -1,16 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import Helmet from 'react-helmet';
-/* { import MongoCall from '../../models/MongoCall/MongoCall'; } */
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
-
-mongoose.connection
-  .on('error', function (err) {
-    console.error('Error connecting to mongodb: ' + err)
-  })
-  .once('open', function () {
-    console.log('Connected to mongodb!');
-  });
+import MongoCall from '../../models/MongoCall/MongoCall';
 
 export default class Register extends Component {
 
