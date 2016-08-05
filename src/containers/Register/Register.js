984e79f74d557745d810ed48aaa2cb5b70dbc98e
sdp <Cobra1985>
2016-08-05 15:48:29 +0200

    final MONGOOSE459a

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import React, {Component, PropTypes} from 'react';
 import Helmet from 'react-helmet';
 /* { import MongoCall from '../../models/MongoCall/MongoCall'; } */
@@ -12,6 +13,7 @@ export default class Register extends Component {
 
   handleMongoCall = (data1, data2) => {
     console.log(data1 + data2);
+    mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
     var userSchema = new mongoose.Schema({
       userid: Number,
       username: String,
