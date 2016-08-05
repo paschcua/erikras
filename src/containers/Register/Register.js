165fb5baba226141363b8d5c9bdc4773e81e9b2f
sdp <Cobra1985>
2016-08-05 14:12:37 +0200

    aha

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -2,29 +2,26 @@ import React, {Component, PropTypes} from 'react';
 import Helmet from 'react-helmet';
 /* { import MongoCall from '../../models/MongoCall/MongoCall'; } */
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
 
-  var mongoose = require('mongoose');
-  mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
+var userSchema = new mongoose.Schema({
+  userid: Number,
+  username: String,
+  password: String
+});
 
-  var userSchema = new mongoose.Schema({
-    userid: Number,
-    username: String,
-    password: String
-  });
+var UserModel = mongoose.model('User', userSchema);
 
-  var UserModel = mongoose.model('User', userSchema);
+var UserData = new UserModel({
+  userid: 4,
+  username: aa,
+  password: bb
+});
 
-  var UserData = new UserModel({
-    userid: 3,
-    username: aa,
-    password: bb
-  });
-
-  UserData.save(function (err) {
-    if (err) return console.log(err);
-  });
-
-
+UserData.save(function (err) {
+  if (err) return console.log(err);
+});
 
 
 export default class Register extends Component {
@@ -35,14 +32,14 @@ export default class Register extends Component {
     logout: PropTypes.func
   }
 
-  handleMongoCall(aa, bb){
-    console.log("DEBUG: "+aa+bb);
+  handleMongoCall(aa, bb) {
+    console.log("DEBUG: " + aa + bb);
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const inputUsername = this.refs.username.value;
-    const inputPassword = this.refs.password.value;
+    var inputUsername = this.refs.username.value;
+    var inputPassword = this.refs.password.value;
     this.handleMongoCall(inputUsername, inputPassword);
   }
 
@@ -51,30 +48,30 @@ export default class Register extends Component {
     const styles = require('./Register.scss');
     return (
       <div className={styles.registerPage + ' container'}>
-        <Helmet title="Register"/>
-        <h1>Registrieren</h1>
-        {!user &&
+      <Helmet title="Register"/>
+      <h1>Registrieren</h1>
+      {!user &&
         <div>
-          <form className="login-form form-inline" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <input type="text" ref="username" placeholder="Username" className="form-control"/>
-            </div>
-            <div className="form-group">
-              <input type="password" ref="password" placeholder="Passwort" className="form-control"/>
-            </div>
-            <button className="btn btn-success" onClick={this.handleSubmit}><i className="fa fa-sign-in"/>{' '}Registrieren</button>
-          </form>
+        <form className="login-form form-inline" onSubmit={this.handleSubmit}>
+        <div className="form-group">
+        <input type="text" ref="username" placeholder="Username" className="form-control"/>
         </div>
-        }
-        {user &&
+        <div className="form-group">
+        <input type="password" ref="password" placeholder="Passwort" className="form-control"/>
+        </div>
+        <button className="btn btn-success" onClick={this.handleSubmit}><i className="fa fa-sign-in"/>{' '}Registrieren</button>
+        </form>
+        </div>
+      }
+      {user &&
         <div>
-          <p>You are currently logged in as username...</p>
+        <p>You are currently logged in as username...</p>
 
-          <div>
-            <button className="btn btn-danger" onClick={logout}><i className="fa fa-sign-out"/>{' '}Log Out</button>
-          </div>
+        <div>
+        <button className="btn btn-danger" onClick={logout}><i className="fa fa-sign-out"/>{' '}Log Out</button>
         </div>
-        }
+        </div>
+      }
       </div>
     );
   }
