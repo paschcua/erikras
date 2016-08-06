9e2d7c4c63a2b1410b0cedb1fc8814fb289421dd
Pascal Scheidegger <paschcua@hispeed.ch>
2016-08-06 15:08:33 +0200

    try again with mongo 2

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -13,8 +13,6 @@ export default class Register extends Component {
 
     console.log('ehm ok: ' + aaa + bbb);
 
-    console.log('proptypes: ' + this.propTypes.user + this.propTypes.login + this.propTypes.logout);
-
     var mongoose = require('mongoose');
     mongoose.connect('mongodb://heroku_r06n6jtm:5jf50mgg9941u4sd42f655q4kb@ds031915.mlab.com:31915/heroku_r06n6jtm');
     console.log('mongoose.connect established: ' + mongoose.connect);
