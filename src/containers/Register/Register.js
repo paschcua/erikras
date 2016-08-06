e32748393e51655f34b68c4873293c28db9e4d1c
Pascal Scheidegger <paschcua@hispeed.ch>
2016-08-06 19:38:23 +0200

    try again with mongo 21

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -21,10 +21,10 @@ export default class Register extends Component {
     .send({ username: inputUsername, password: inputPassword })
     .set('Accept', 'application/json')
     .end(function(err, res){
-      console.log(JSON.stringify(res.body));
+      console.log(JSON.stringify(res.body.first));
       console.log("aha 1"+res.text.second);
-      console.log(res.body[0].first);
-      console.log("aha 2"+res.text.first);
+      console.log(res.body.first);
+      console.log("aha 2"+JSON.stringify(res.body.first));
     });
   }
 
