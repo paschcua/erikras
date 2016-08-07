509ee9828c744776e5b1bd5c3153eeeb79b3a60b
Pascal Scheidegger <paschcua@hispeed.ch>
2016-08-07 21:53:27 +0200

    working state 07.08.16 1aaag

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -77,6 +77,7 @@ export default class Register extends Component {
           <Label bsStyle="danger">Fehler</Label><br /><br />
           {formMsg}
         </div>
+        : null
         }
       </div>
     );
