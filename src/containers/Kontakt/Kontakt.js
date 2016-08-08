853bc59f366c8cfcd7126935c43a2d76f3ad1d83
Pascal Scheidegger <paschcua@hispeed.ch>
2016-08-08 22:19:04 +0200

    test 08.08.16 v8.7

--- src/containers/Kontakt/Kontakt.js
+++ src/containers/Kontakt/Kontakt.js
@@ -4,15 +4,12 @@ import Helmet from 'react-helmet';
 export default class Kontakt extends Component {
 
   render() {
-    const styles = require('./Kontakt.scss');
     return (
-      <div className={styles.kontakt}>
         <div className="container">
           <h1>Kontakt...</h1>
           <Helmet title="Kontakt"/>
           <p>Kontakt will open soon...</p>
         </div>
-      </div>
     );
   }
 
