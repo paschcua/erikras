5af4df0678fd5509314d8fe3f8decf887e5e68cb
Pascal Scheidegger <paschcua@hispeed.ch>
2016-09-12 22:43:02 +0200

    aha aha aha 2

--- src/redux/actions/getDataActions.js
+++ src/redux/actions/getDataActions.js
@@ -1,11 +1,13 @@
 export function fetchItFn(fetchit, email, pw, uuid) {
-  return {
-    type: 'ADD_USER_MAN',
-    payload: {
-      fetchit,
-      email,
-      pw,
-      uuid,
-    }
+  return function(dispatch) {
+    dispatch({
+      type: 'ADD_USER_MAN_FULFILLED',
+      payload: {
+        fetchit: fetchit,
+        email: email,
+        pw: pw,
+        uuid: uuid,
+      }
+    });
   };
 }
