3a81ac17b3636c494a3f6e2d528d7b8f0d77c0e3
Pascal Scheidegger <paschcua@hispeed.ch>
2016-09-18 22:43:23 +0200

    mailer test v22 DEF v2

--- src/server.js
+++ src/server.js
@@ -100,8 +100,8 @@ app.post('/registrieren', function(req, res) {
                         <tr>
                           <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
                               <tr>
-                                <td width="61"><a href= "//www.swiss-react.ch" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_01_01.jpg" width="61" height="76" border="0" alt=""/></a></td>
-                                <td width="144"><a href= "//www.swiss-react.ch" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_01_02.jpg" width="144" height="76" border="0" alt=""/></a></td>
+                                <td width="61"><a href= "//www.swiss-react.ch" target="_blank"><img src="http://www.swiss-react.ch/nl/images/PROMO-GREEN2_01_01.jpg" width="61" height="76" border="0" alt=""/></a></td>
+                                <td width="144"><a href= "//www.swiss-react.ch" target="_blank"><img src="http://www.swiss-react.ch/nl/images/PROMO-GREEN2_01_02.jpg" width="144" height="76" border="0" alt=""/></a></td>
                                 <td width="393"><table width="100%" border="0" cellspacing="0" cellpadding="0">
                                     <tr>
                                       <td height="46" align="right" valign="middle"><table width="100%" border="0" cellspacing="0" cellpadding="0">
@@ -112,7 +112,7 @@ app.post('/registrieren', function(req, res) {
                                         </table></td>
                                     </tr>
                                     <tr>
-                                      <td height="30"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_01_04.jpg" width="393" height="30" border="0" alt=""/></td>
+                                      <td height="30"><img src="http://www.swiss-react.ch/nl/images/PROMO-GREEN2_01_04.jpg" width="393" height="30" border="0" alt=""/></td>
                                     </tr>
                                   </table></td>
                               </tr>
@@ -148,7 +148,7 @@ app.post('/registrieren', function(req, res) {
                           <td>&nbsp;</td>
                         </tr>
                         <tr>
-                          <td><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_07.jpg" width="598" height="7" style="display:block" border="0" alt=""/></td>
+                          <td><img src="http://www.swiss-react.ch/nl/images/PROMO-GREEN2_07.jpg" width="598" height="7" style="display:block" border="0" alt=""/></td>
                         </tr>
                         <tr>
                           <td>&nbsp;</td>
@@ -164,9 +164,9 @@ app.post('/registrieren', function(req, res) {
                                 <td width="11%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "//www.swiss-react.ch" style="color:#010203; text-decoration:none"><strong>CONTACT </strong></a></font></td>
                                 <td width="2%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>|</strong></font></td>
                                 <td width="17%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>STAY CONNECTED:</strong></font></td>
-                                <td width="4%" align="right"><a href="https://www.facebook.com/" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_09_01.jpg" alt="facebook" width="21" height="19" border="0" /></a></td>
-                                <td width="5%" align="center"><a href="https://twitter.com/" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_09_02.jpg" alt="twitter" width="21" height="19" border="0" /></a></td>
-                                <td width="4%" align="right"><a href="http://www.linkedin.com/" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_09_03.jpg" alt="linkedin" width="21" height="19" border="0" /></a></td>
+                                <td width="4%" align="right"><a href="https://www.facebook.com/" target="_blank"><img src="http://www.swiss-react.ch/nl/images/PROMO-GREEN2_09_01.jpg" alt="facebook" width="21" height="19" border="0" /></a></td>
+                                <td width="5%" align="center"><a href="https://twitter.com/" target="_blank"><img src="http://www.swiss-react.ch/nl/images/PROMO-GREEN2_09_02.jpg" alt="twitter" width="21" height="19" border="0" /></a></td>
+                                <td width="4%" align="right"><a href="http://www.linkedin.com/" target="_blank"><img src="http://www.swiss-react.ch/nl/images/PROMO-GREEN2_09_03.jpg" alt="linkedin" width="21" height="19" border="0" /></a></td>
                                 <td width="5%">&nbsp;</td>
                               </tr>
                             </table></td>
