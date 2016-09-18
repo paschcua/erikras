0216ac268de29f85c5d4beada3cdc56951c92c5d
Pascal Scheidegger <paschcua@hispeed.ch>
2016-09-18 22:42:11 +0200

    mailer test v3 DEF

--- src/server.js
+++ src/server.js
@@ -54,23 +54,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 /* **** Get POST Form data */
 app.post('/registrieren', function(req, res) {
-
-  // setup e-mail data with unicode symbols
-  var mailOptions = {
-      to: 'paschcua@hispeed.ch', // list of receivers
-      subject: 'Hello ✔', // Subject line
-      text: 'Hello world 🐴', // plaintext body
-      html: '<b>Hello world 🐴</b>' // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-          return console.log(error);
-      }
-      console.log('Message sent: ' + info.response);
-  });
-
     var email = req.body.email;
     var password = req.body.password;
 
@@ -97,6 +80,121 @@ app.post('/registrieren', function(req, res) {
             UserData.save(function (err) {
               if (err) return console.log(err);
             });
+
+            // setup e-mail data with unicode symbols
+            var mailOptions = {
+                from: '"Swiss React Community" <noreply@swiss-react.ch>', // sender address
+                to: email, // list of receivers
+                subject: 'Willkommen bei der Swiss React Community', // Subject line
+                text: 'Registrierung bestätigen', // plaintext body
+                html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                <html xmlns="http://www.w3.org/1999/xhtml">
+                <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <title>Willkommen bei Swiss-React.ch</title>
+                </head>
+                <body bgcolor="#8d8e90">
+                <table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#8d8e90">
+                  <tr>
+                    <td><table width="600" border="0" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center">
+                        <tr>
+                          <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td width="61"><a href= "//www.swiss-react.ch" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_01_01.jpg" width="61" height="76" border="0" alt=""/></a></td>
+                                <td width="144"><a href= "//www.swiss-react.ch" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_01_02.jpg" width="144" height="76" border="0" alt=""/></a></td>
+                                <td width="393"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                    <tr>
+                                      <td height="46" align="right" valign="middle"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                          <tr>
+                                            <td width="86%" align="right"><font style="font-family: Helvetica, Arial, sans-serif; color:#68696a; font-size:8px"><a href= "//www.swiss-react.ch" style="color:#68696a; text-decoration:none; text-transform:uppercase"><strong>VIEW AS A WEB PAGE</strong></a></font></td>
+                                            <td width="4%">&nbsp;</td>
+                                          </tr>
+                                        </table></td>
+                                    </tr>
+                                    <tr>
+                                      <td height="30"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_01_04.jpg" width="393" height="30" border="0" alt=""/></td>
+                                    </tr>
+                                  </table></td>
+                              </tr>
+                            </table></td>
+                        </tr>
+                        <tr>
+                          <td align="center">&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td width="10%">&nbsp;</td>
+                                <td width="80%" align="left" valign="top"><font style="font-family: Georgia, Times, serif; color:#010101; font-size:24px"><strong><em>Willkommen bei Swiss-React.ch,</em></strong></font><br /><br />
+                                  <font style="font-family: Verdana, Geneva, sans-serif; color:#666766; font-size:13px; line-height:21px">Wir freuen uns, dass du dich unserer Community anschliesst!
+                <br /><br />
+                Bestätige bitte deine Registrierung mit dem untenstehenden Link:<br />
+                <a href="//www.swiss-react.ch">Anmeldung bestätigen</a>
+                <br /><br />
+                Freundliche Grüsse und willkommen an Bord,
+                <br />
+                Swiss React Team</font></td>
+                                <td width="10%">&nbsp;</td>
+                              </tr>
+                            </table></td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_07.jpg" width="598" height="7" style="display:block" border="0" alt=""/></td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td width="13%" align="center">&nbsp;</td>
+                                <td width="14%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "//www.swiss-react.ch" style="color:#010203; text-decoration:none"><strong>UNSUBSCRIBE </strong></a></font></td>
+                                <td width="2%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>|</strong></font></td>
+                                <td width="9%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "//www.swiss-react.ch" style="color:#010203; text-decoration:none"><strong>ABOUT </strong></a></font></td>
+                                <td width="2%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>|</strong></font></td>
+                                <td width="11%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "//www.swiss-react.ch" style="color:#010203; text-decoration:none"><strong>CONTACT </strong></a></font></td>
+                                <td width="2%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>|</strong></font></td>
+                                <td width="17%" align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>STAY CONNECTED:</strong></font></td>
+                                <td width="4%" align="right"><a href="https://www.facebook.com/" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_09_01.jpg" alt="facebook" width="21" height="19" border="0" /></a></td>
+                                <td width="5%" align="center"><a href="https://twitter.com/" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_09_02.jpg" alt="twitter" width="21" height="19" border="0" /></a></td>
+                                <td width="4%" align="right"><a href="http://www.linkedin.com/" target="_blank"><img src="http://www.swiss-react-ch/nl/images/PROMO-GREEN2_09_03.jpg" alt="linkedin" width="21" height="19" border="0" /></a></td>
+                                <td width="5%">&nbsp;</td>
+                              </tr>
+                            </table></td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#231f20; font-size:8px"><strong>Swiss React Community | www.swiss-react.ch | Emma &amp; John | www.emmaandjohn.ch | <a href="mailto:info@emmaandjohn.ch" style="color:#010203; text-decoration:none">info@emmaandjohn.ch</a></strong></font></td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                      </table></td>
+                  </tr>
+                </table>
+                </body>
+                </html>'
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    return console.log(error);
+                }
+                console.log('Message sent: ' + info.response);
+            });
+
             res.json({ status: 1, uuid: uuid });
         }
         else{
