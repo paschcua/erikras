4321ea8e0091427b1ac0c08179e426d2d5057e7c
Pascal Scheidegger <paschcua@hispeed.ch>
2016-09-20 16:45:05 +0200

    tester 23aaaa SUPER

--- src/server.js
+++ src/server.js
@@ -19,16 +19,10 @@ import createHistory from 'react-router/lib/createMemoryHistory';
 import {Provider} from 'react-redux';
 import getRoutes from './routes';
 import cookieParser from 'cookie-parser';
+import nodemailer from 'nodemailer';
 
-var nodemailer = require('nodemailer');
-
-const smtpTransport = nodemailer.createTransport("SMTP",{
-   service: "Gmail",
-   auth: {
-       user: "paschcua@gmail.com",
-       pass: "cobra1985"
-   }
-});
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtps://paschcua%40gmail.com:cobra1985@smtp.gmail.com');
 
 const targetUrl = 'http://' + config.apiHost + ':' + config.apiPort;
 const pretty = new PrettyError();
@@ -61,17 +55,21 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 /* **** Get POST Form data */
 app.post('/registrieren', function(req, res) {
 
-    smtpTransport.sendMail({
-       to: "paschcua@hispeed.ch",
-       subject: "Emailing with nodemailer",
-       text: "Email Example with nodemailer"
-    }, function(error, response){
-       if(error){
-           console.log(error);
-       }else{
-           console.log("Message sent: " + response.message);
-       }
-    });
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+      to: 'paschcua@hispeed.ch', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world 🐴', // plaintext body
+      html: '<b>Hello world 🐴</b>' // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+  });
 
     var email = req.body.email;
     var password = req.body.password;
@@ -99,7 +97,6 @@ app.post('/registrieren', function(req, res) {
             UserData.save(function (err) {
               if (err) return console.log(err);
             });
-
             res.json({ status: 1, uuid: uuid });
         }
         else{
