8d2076f38532b4717ab4081fcf42c11995330f14
sdp <Cobra1985>
2016-10-05 14:32:43 +0200

    editor

--- src/containers/Community/Community.js
+++ src/containers/Community/Community.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import Editor from 'draft-js-editor'
 import Helmet from 'react-helmet';
 import cookie from 'react-cookie';
 import { connect } from 'react-redux';
@@ -14,34 +14,21 @@ export default class MyEditor extends Component {
   state = {
     loginEmail: cookie.load('ck_email')
   }
-  constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
-  }
-  handleKeyCommand(command) {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
-    }
-    return 'not-handled';
-  }
 
   render() {
+    const style = require('./Community.scss');
     const {loginEmail} = this.state;
     return (
       <div className="container">
         <h1>Community</h1>
         <Helmet title="Community"/>
-        <div className="editor">
-          <Editor
-            editorState={this.state.editorState}
-            handleKeyCommand={this.handleKeyCommand}
-            onChange={this.onChange}
-          />
-        </div>
+        <div>
+              A sample text editor
+              <Editor
+                onChange={(editorState) => this.setState({ editorState })}
+                editorState={this.state.editorState}
+              />
+            </div>
       </div>
     );
   }
