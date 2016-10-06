ae54e3613a61c994fc8871a98cf70561754a1a13
Pascal Scheidegger <paschcua@hispeed.ch>
2016-10-06 18:51:54 +0200

    editor test new

--- src/containers/Community/Community.js
+++ src/containers/Community/Community.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Editor from 'draft-js-editor'
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import Helmet from 'react-helmet';
 import cookie from 'react-cookie';
 import { connect } from 'react-redux';
@@ -10,25 +10,98 @@ import { connect } from 'react-redux';
   };
 })
 
-export default class MyEditor extends Component {
+export default class RichEditorExample extends Component {
   state = {
     loginEmail: cookie.load('ck_email')
   }
 
+  constructor(props) {
+            super(props);
+            this.state = {editorState: EditorState.createEmpty()};
+
+            this.focus = () => this.refs.editor.focus();
+            this.onChange = (editorState) => this.setState({editorState});
+
+            this.handleKeyCommand = (command) => this._handleKeyCommand(command);
+            this.onTab = (e) => this._onTab(e);
+            this.toggleBlockType = (type) => this._toggleBlockType(type);
+            this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
+          }
+
+          _handleKeyCommand(command) {
+            const {editorState} = this.state;
+            const newState = RichUtils.handleKeyCommand(editorState, command);
+            if (newState) {
+              this.onChange(newState);
+              return true;
+            }
+            return false;
+          }
+
+          _onTab(e) {
+            const maxDepth = 4;
+            this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
+          }
+
+          _toggleBlockType(blockType) {
+            this.onChange(
+              RichUtils.toggleBlockType(
+                this.state.editorState,
+                blockType
+              )
+            );
+          }
+
+          _toggleInlineStyle(inlineStyle) {
+            this.onChange(
+              RichUtils.toggleInlineStyle(
+                this.state.editorState,
+                inlineStyle
+              )
+            );
+          }
+
+
   render() {
     const style = require('./Community.scss');
-    const {loginEmail} = this.state;
+    const {loginEmail, editorState} = this.state;
+    // If the user changes block type before entering any text, we can
+    // either style the placeholder or hide it. Let's just hide it now.
+              let className = 'RichEditor-editor';
+              var contentState = editorState.getCurrentContent();
+              if (!contentState.hasText()) {
+                if (contentState.getBlockMap().first().getType() !== 'unstyled') {
+                  className += ' RichEditor-hidePlaceholder';
+                }
+              }
+
     return (
       <div className="container">
         <h1>Community</h1>
         <Helmet title="Community"/>
-        <div>
-              A sample text editor
-              <Editor
-                onChange={(editorState) => this.setState({ editorState })}
-                editorState={this.state.editorState}
-              />
-            </div>
+          <div className="RichEditor-root">
+                       <BlockStyleControls
+                         editorState={editorState}
+                         onToggle={this.toggleBlockType}
+                       />
+                       <InlineStyleControls
+                         editorState={editorState}
+                         onToggle={this.toggleInlineStyle}
+                       />
+                       <div className={className} onClick={this.focus}>
+                         <Editor
+                           blockStyleFn={getBlockStyle}
+                           customStyleMap={styleMap}
+                           editorState={editorState}
+                           handleKeyCommand={this.handleKeyCommand}
+                           onChange={this.onChange}
+                           onTab={this.onTab}
+                           placeholder="Tell a story..."
+                           ref="editor"
+                           spellCheck={true}
+                         />
+                       </div>
+         </div>
       </div>
     );
   }
