583435c808a976b252d7115378f1bbdeaf488efb
Pascal Scheidegger <paschcua@hispeed.ch>
2016-10-09 19:34:42 +0200

    blog mongoose 3.23vsss

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -9,6 +9,7 @@ import {reducer as form} from 'redux-form';
 import info from './info';
 import widgets from './widgets';
 import registerNewUser from './registerNewUserReducer';
+import activateNewUser from './activateNewUserReducer';
 
 export default combineReducers({
   routing: routerReducer,
@@ -22,5 +23,6 @@ export default combineReducers({
   }),
   info,
   widgets,
-  registerNewUser
+  registerNewUser,
+  activateNewUser
 });
