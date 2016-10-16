9bcf4aba116ded7a4ffd6b00959cb8d0d085ce52
Pascal Scheidegger <paschcua@hispeed.ch>
2016-10-16 18:12:29 +0200

    persistent data v1

--- src/redux/actions/registerNewUserActions.js
+++ src/redux/actions/registerNewUserActions.js
@@ -1,12 +1,12 @@
-export function registerNewUser(fetchit, email, pw, uuid) {
+export function registerNewUser(activation, email, pw, uuid) {
   return function(dispatch) {
     dispatch({
       type: 'REGISTER_NEW_USER_FULFILLED',
       payload: {
+        activation: activation
         email: email,
         pw: pw,
         uuid: uuid,
-        activation: false
       }
     });
   };
